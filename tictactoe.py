aa1083616f61015c2cb35a833ca6094360027320
Amos P <amosp323@gmail.com>
2020-10-15 15:16:08 +0530

    Edit tictactoe.py
    - Fix issue in print_board

--- tictactoe.py
+++ tictactoe.py
@@ -1,21 +1,21 @@
 class Board:
     def __init__(self):
-        self.top_left = ''
-        self.top_center = ''
-        self.top_right = ''
-        self.middle_left = ''
-        self.middle_center = ''
-        self.middle_right = ''
-        self.bottom_left = ''
-        self.bottom_center = ''
-        self.bottom_right = ''
+        self.top_left = ' '
+        self.top_center = ' '
+        self.top_right = ' '
+        self.middle_left = ' '
+        self.middle_center = ' '
+        self.middle_right = ' '
+        self.bottom_left = ' '
+        self.bottom_center = ' '
+        self.bottom_right = ' '
 
     def print_board(self):
         print('{} | {} | {}'.format(self.top_left, self.top_center, self.top_right))
-        print('------')
-        print('{} | {} | {}'.format(self.top_left, self.top_center, self.top_right))
-        print('------')
-        print('{} | {} | {}'.format(self.top_left, self.top_center, self.top_right))
+        print(' -------- ')
+        print('{} | {} | {}'.format(self.middle_left, self.middle_center, self.middle_right))
+        print(' -------- ')
+        print('{} | {} | {}'.format(self.bottom_left, self.bottom_center, self.bottom_right))
 
 game_board = Board()
 
