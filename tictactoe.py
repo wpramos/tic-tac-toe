cef2cf89ba824c884ed7fe1d08af44e6a0f1e63f
Amos P <amosp323@gmail.com>
2020-10-15 15:07:21 +0530

    Add tictactoe.py
    Create class Board
    Define print_board

---
+++ tictactoe.py
@@ -0,0 +1,22 @@
+class Board:
+    def __init__(self):
+        self.top_left = ''
+        self.top_center = ''
+        self.top_right = ''
+        self.middle_left = ''
+        self.middle_center = ''
+        self.middle_right = ''
+        self.bottom_left = ''
+        self.bottom_center = ''
+        self.bottom_right = ''
+
+    def print_board(self):
+        print('{} | {} | {}'.format(self.top_left, self.top_center, self.top_right))
+        print('------')
+        print('{} | {} | {}'.format(self.top_left, self.top_center, self.top_right))
+        print('------')
+        print('{} | {} | {}'.format(self.top_left, self.top_center, self.top_right))
+
+game_board = Board()
+
+game_board.print_board()
