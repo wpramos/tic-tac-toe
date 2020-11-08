3c3421ed860840b08def20f8b57a0a077c45d5cb
Amos P <amosp323@gmail.com>
2020-11-08 15:39:44 +0530

    Improve PEP8 compliance of test_tictactoe.py

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -2,71 +2,80 @@ import unittest
 
 from tictactoe import Box, Board
 
+
 class TestFindWinner(unittest.TestCase):
     def setUp(self):
         self.sample_board = Board()
 
     def tearDown(self):
         pass
-        
+
     def test_find_winner_diagonal_X(self):
         self.sample_board.top_left.mark = 'X'
         self.sample_board.middle_center.mark = 'X'
         self.sample_board.bottom_right.mark = 'X'
-        
+
         self.sample_board.print_board()
 
-        self.assertEqual(self.sample_board.find_winner(), 'X', "Should be 'X'.")
+        self.assertEqual(self.sample_board.find_winner(),
+                         'X', "Should be 'X'.")
 
     def test_find_winner_diagonal_O(self):
         self.sample_board.top_right.mark = 'O'
         self.sample_board.middle_center.mark = 'O'
         self.sample_board.bottom_left.mark = 'O'
-        
+
         self.sample_board.print_board()
 
-        self.assertEqual(self.sample_board.find_winner(), 'O', "Should be 'O'.")
+        self.assertEqual(self.sample_board.find_winner(),
+                         'O', "Should be 'O'.")
 
     def test_find_winner_row_X(self):
         self.sample_board.top_left.mark = 'X'
         self.sample_board.top_center.mark = 'X'
         self.sample_board.top_right.mark = 'X'
-        
+
         self.sample_board.print_board()
 
-        self.assertEqual(self.sample_board.find_winner(), 'X', "Should be 'X'.")
+        self.assertEqual(self.sample_board.find_winner(),
+                         'X', "Should be 'X'.")
 
     def test_find_winner_row_O(self):
         self.sample_board.bottom_left.mark = 'O'
         self.sample_board.bottom_center.mark = 'O'
         self.sample_board.bottom_right.mark = 'O'
-        
+
         self.sample_board.print_board()
 
-        self.assertEqual(self.sample_board.find_winner(), 'O', "Should be 'O'.")
+        self.assertEqual(self.sample_board.find_winner(),
+                         'O', "Should be 'O'.")
 
     def test_find_winner_column_X(self):
         self.sample_board.top_left.mark = 'X'
         self.sample_board.middle_left.mark = 'X'
         self.sample_board.bottom_left.mark = 'X'
-        
+
         self.sample_board.print_board()
 
-        self.assertEqual(self.sample_board.find_winner(), 'X', "Should be 'X'.")
+        self.assertEqual(self.sample_board.find_winner(),
+                         'X', "Should be 'X'.")
 
     def test_find_winner_column_O(self):
         self.sample_board.top_right.mark = 'O'
         self.sample_board.middle_right.mark = 'O'
         self.sample_board.bottom_right.mark = 'O'
-        
+
         self.sample_board.print_board()
 
-        self.assertEqual(self.sample_board.find_winner(), 'O', "Should be 'O'.")
+        self.assertEqual(self.sample_board.find_winner(),
+                         'O', "Should be 'O'.")
 
     def test_find_winner_blank(self):
         self.sample_board.print_board()
-        
-        self.assertEqual(self.sample_board.find_winner(), None, 'Should return None.')
-    
+
+        self.assertEqual(self.sample_board.find_winner(),
+                         None, 'Should return None.')
+
+
 if __name__ == "__main__":
     unittest.main()
